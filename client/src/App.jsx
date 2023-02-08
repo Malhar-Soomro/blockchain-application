e6c2cfe3766c86fa1f1d08d126469c1ca715f81f
Malhar-Soomro <malharsoomro2@gmail.com>
2023-02-08 18:48:08 +0500

    navbar developed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {Navbar, Welcome, Transactions, Services, Footer} from "./components"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
-    <div className="App">
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+        <Services/>
+        <Transactions/>
+        <Footer/>
     </div>
+
   )
 }
 
