b39e99c50e12283f0caa75d388b2ef858421a62b
Malhar-Soomro <malharsoomro2@gmail.com>
2023-02-23 09:59:52 +0500

    set width full of container of those two childs

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -9,7 +9,7 @@ const ServiceCard = ({color, title, icon, subtitle}) => (
     </div>
     <div className="ml-5 flex flex-col flex-1">
       <h1 className="mt-2 text-white text-lg">{title}</h1>
-      <p className="mt-2 text-white text-sm md:w-9/12">{subtitle}</p>
+      <p className="mt-2 text-white text-sm w-9/12">{subtitle}</p>
     </div>
   </div>
 );
@@ -17,7 +17,7 @@ const ServiceCard = ({color, title, icon, subtitle}) => (
 const Services = () => {
     return(
        <div className="flex w-full justify-center items-center gradient-bg-services">
-         <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+         <div className="flex w-full mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
            <div className="flex flex-col flex-1">
              <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
                 Services that we
