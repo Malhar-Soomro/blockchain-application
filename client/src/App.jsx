949acad541c38ae616de4770a636c0f259fc3fa7
Malhar-Soomro <malharsoomro2@gmail.com>
2023-03-01 23:30:25 +0500

    navbar developed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
-import './App.css'
+import { Navbar, Welcome, Services, Transactions, Footer } from "./components"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <div className='gradient-bg-welcome'>
+      <Navbar/>
+      <Welcome/>
+      <Services/>
+      <Transactions/>
+      <Footer/>
+   </div>
   );
 }
 export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import If, {Else} from "if-else-react";
+
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className='flex items-start md:justify-evenly justify-between gradient-bg-navbar p-4'>
+      <div>
+        <img src={logo} alt="logo" className='w-32' />
+      </div>
+
+      <div className='md:flex items-center justify-between hidden'>
+        <p className='text-white text-base mx-4'>Market</p>
+        <p className='text-white text-base mx-4'>Exchange</p>
+        <p className='text-white text-base mx-4'>Tutorials</p>
+        <p className='text-white text-base mx-4'>Wallets</p>
+        <button className='flex bg-[#2952e3] rounded-full mx-2'>
+          <p className='text-white text-base py-2 px-7'>Login</p>
+        </button>
+
+      </div>
+        
+        <div className='flex flex-col items-end'>
+
+        <If condition={toggle}>
+          <AiOutlineClose 
+          onClick={()=>setToggle(false)} 
+          className='text-white md:hidden' 
+          fontSize={30}/>
+
+        <Else/>
+           <HiMenuAlt4 onClick={()=>setToggle(true)}
+           className='text-white md:hidden'
+           fontSize={30}/>
+        </If>
+
+        <If condition={toggle}>
+
+          <div className='flex flex-col gap-4 bg h-screen fixed top-0 right-0 blue-glassmorphism w-[70vw] transition duration-500 ease-in-out shadow-2xl p-5 md:hidden animate-slide-in rounded-md'>
+          <AiOutlineClose 
+          onClick={()=>setToggle(false)} 
+          className='text-white md:hidden' 
+          fontSize={21}/>
+            <p className='text-white text-lg text-right'>Market</p>
+            <p className='text-white text-lg text-right'>Exchange</p>
+            <p className='text-white text-lg text-right'>Tutorials</p>
+            <p className='text-white text-lg text-right'>Wallets</p>
+          </div>  
+        </If>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
